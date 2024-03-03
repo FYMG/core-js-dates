6b39bd2af710a99e7152a6abc8891d482cd2a31b
FYMG <34478249+FYMG@users.noreply.github.com>
2024-03-03 15:42:24 +0300

    feat: implemented getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -63,8 +63,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date.getTime());
+  result.setDate(result.getDate() + ((7 - result.getDay() + 5) % 7 || 7));
+  return result;
 }
 
 /**
